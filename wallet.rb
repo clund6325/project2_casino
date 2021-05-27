5a351b52cd3283d79e6179914ecff83692612612
Katie Secrest <katie.secrest@gmail.com>
2021-05-27 12:13:46 -0600

    fixed bug with wallet

--- wallet.rb
+++ wallet.rb
@@ -1,9 +1,9 @@
 class  Wallet
   def initialize(money)
-    @wallet = money.to_f
+    @wallet = money
   end
   def validate_money(spending_money)
-    if @wallet.to_f - spending_money.to_f >= 0
+    if @wallet - spending_money >= 0
       true
     else
       puts "You don't have enough money! Get out of here, ya bum!!"
@@ -12,14 +12,19 @@ class  Wallet
   end
 
   def subtract_money(money)
-    @wallet -= money.to_f
+    @wallet -= money
   end
 
   def add_money(money)
-    @wallet += money.to_f
+    @wallet += money
   end
 
   def print_balance
     puts "Your current balance is $#{@wallet}".colorize(:green)
   end
+
+  def get_wallet
+    @wallet
+  end
+
 end
